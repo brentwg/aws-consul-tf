db19a307db0ef4dda70e77e3bf82e53b3253c21f
Brent Gawryluik <brentwg@zoho.com>
2018-02-20 11:22:17 -0800

    Added iam module outputs

--- modules/consul-client-iam/outputs.tf
+++ modules/consul-client-iam/outputs.tf
@@ -1,3 +1,25 @@
 # --------------
 # Module Outputs
 # --------------
+
+# Role Outputs
+output "consul_client_role_id" {
+  value = "${aws_iam_role.consul_client_role.id}"
+}
+
+output "consul_client_role_name" {
+  value = "${aws_iam_role.consul_client_role.name}"
+}
+
+output "consul_client_role_arn" {
+  value = "${aws_iam_role.consul_client_role.arn}"
+}
+
+# Profile Outputs
+output "consul_client_profile_arn" {
+  value = "${aws_iam_instance_profile.consul_client_profile.arn}"
+}
+
+output "consul_client_profile_name" {
+  value = "${aws_iam_instance_profile.consul_client_profile.name}"
+}
